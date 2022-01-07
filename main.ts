0093a7fa571b3a1eae7114de9a9a327972ad55d2
Sooun <julien.malenge@yahoo.fr>
2022-01-07 09:35:26 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,14 @@ pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+// Facteur de vitesse des moteurs , compris entre 0 et 100
+let K_vitesse = 100
+// Poids du second moteur. Compris entre 50 et 0.
+// 50 => On tourne très peu
+// 18 => Bon compromis
+// 10 => On tourne très fort.
+// 0 => Jostick directionnel ON/OFF
+let K_joystick = 18
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
         basic.showString("" + convertToText(Motor_G) + ";" + convertToText(Moteur_D))
@@ -24,21 +32,23 @@ basic.forever(function () {
         x = Math.round(Math.map(X_valeur, 0, 1020, -100, 100))
         y = Math.round(Math.map(Y_valeur, 0, 1020, -100, 100))
         if ((X_valeur <= 480 || Y_valeur >= 540) && (X_valeur < 510 && Y_valeur >= 510)) {
-            Motor_G = Math.round(y / 1 - Math.abs(y * x) * 0.0082)
+            Motor_G = Math.round(y / 1 - Math.abs(y * x) * ((100 - K_joystick) / 10000))
             Moteur_D = Math.max(Math.abs(x), y)
         } else if ((X_valeur >= 540 || Y_valeur >= 540) && (X_valeur >= 510 && Y_valeur > 510)) {
             Motor_G = Math.max(x, y)
-            Moteur_D = Math.round(y / 1 - y * x * 0.0082)
+            Moteur_D = Math.round(y / 1 - y * x * ((100 - K_joystick) / 10000))
         } else if ((X_valeur <= 480 || Y_valeur <= 480) && (X_valeur > 510 && Y_valeur <= 500)) {
             Motor_G = Math.min(-1 * x, y)
-            Moteur_D = Math.round(y / 1 - y * x * 0.0082)
+            Moteur_D = Math.round(y / 1 - y * x * ((100 - K_joystick) / 10000))
         } else if ((X_valeur >= 540 || Y_valeur <= 480) && (X_valeur <= 510 && Y_valeur < 500)) {
             Moteur_D = Math.min(x, y)
-            Motor_G = Math.round(y / 1 + y * x * 0.0082)
+            Motor_G = Math.round(y / 1 + y * x * ((100 - K_joystick) / 10000))
         } else {
             Motor_G = 0
             Moteur_D = 0
         }
+        Motor_G = Motor_G * K_vitesse * 25
+        Moteur_D = Moteur_D * K_vitesse * 25
         radio.sendString("" + convertToText(Motor_G) + ";" + convertToText(Moteur_D))
     }
 })
